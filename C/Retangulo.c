1a45d3884daf738263b1544c7c3466b7c3f3a352
Marcela Marcellino Querós <143046716+MarcelaMQueros@users.noreply.github.com>
2024-09-07 22:11:07 -0300

    Add files via upload

---
+++ C/Retangulo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+void limpar_entrada()
+{
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+int main() {
+    double base, altura, area, perimetro, diagonal;
+
+    printf("Base do retangulo: ");
+    scanf("%lf", &base);
+    limpar_entrada();
+
+    printf("Altura do retangulo: ");
+    scanf("%lf", &altura);
+
+    area = altura * base;
+    perimetro = 2 * (altura + base);
+    diagonal = sqrt(pow(altura,2) + pow(base, 2));
+
+    printf("AREA = %.4lf \nPERIMETRO = %.4lf \nDIAGONAL = %.4lf", area, perimetro, diagonal);
+
+
+
+
+
+
+}
