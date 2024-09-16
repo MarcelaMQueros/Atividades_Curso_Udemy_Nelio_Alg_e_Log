63192ed7025f1fb118defd8784cc7010891651af
Marcela Marcellino Querós <143046716+MarcelaMQueros@users.noreply.github.com>
2024-09-16 13:22:23 -0300

    Add files via upload

--- C/Temperatura.c
+++ C/Temperatura.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
-
 int main(){
+    double temperatura, temperatura_convertida;
+    char escala;
 
+    printf("Voce vai digitar a temperatura em qual escala (C/F)? ");
+    scanf("%c", &escala);
+
+    if (escala == 'F') {
+        printf("Digite a temperatura em Fahrenheit: ");
+        scanf("%lf", &temperatura);
+        temperatura_convertida = 5.00 / 9.00*(temperatura - 32);
+        printf("Temperatura equivalente em Celsius: %.2lf", temperatura_convertida);
+    }else{
+        printf("Digite a temperatura em  Celsius: ");
+        scanf("%lf", &temperatura);
+        temperatura_convertida = temperatura * 9 / 5 + 32;
+        printf("Temperatura equivalente em Fahrenheit: %.2lf", temperatura_convertida);
+    }
 }
+
