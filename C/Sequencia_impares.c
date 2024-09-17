d5bb61413a8b7a71e47b7c54aa0accda1d0227c3
Marcela Marcellino Querós <143046716+MarcelaMQueros@users.noreply.github.com>
2024-09-17 09:11:58 -0300

    Add files via upload

---
+++ C/Sequencia_impares.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int x;
+    printf("Digite o valor de X: ");
+    scanf("%d", &x);
+    for (int i = 1; i < x+1; i++){
+        if (i % 2 != 0) {
+            printf("%d\n", i);
+        }
+    }
+return 0;
+}
