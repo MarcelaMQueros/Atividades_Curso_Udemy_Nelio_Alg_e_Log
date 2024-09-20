69df5b23ce747a1c9be6b43fa84236d14da16277
Marcela Marcellino Querós <143046716+MarcelaMQueros@users.noreply.github.com>
2024-09-20 10:45:36 -0300

    Add files via upload

---
+++ C++/Retangulo.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    double base, altura, area, perimetro, diagonal;
+
+    cout << "Base do retangulo: ";
+    cin >> base;
+    cout << "Altura do retangulo: ";
+    cin >> altura;
+
+    area = base * altura;
+    perimetro = 2 * (base + altura);
+    diagonal = sqrt(pow(base, 2.0) + pow(altura, 2.0));
+    cout << fixed << setprecision(4);
+    cout << "AREA = " << area << "\nPERIMETRO = " << perimetro << "\nDIAGONAL = " << diagonal << endl;
+
+
+
+}
+
