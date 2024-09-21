af8ca65f9340d65c8dd7a93e1d91015e9e3f6b4c
Marcela Marcellino Querós <143046716+MarcelaMQueros@users.noreply.github.com>
2024-09-21 20:25:25 -0300

    Add files via upload

---
+++ C#/Crescente/Crescente/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Crescente {
+    class Crescente {
+        static void Main(String[] args) {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            int num1, num2;
+
+            do {
+                Console.WriteLine("Digite dois numeros: ");
+                num1 = int.Parse(Console.ReadLine());
+                num2 = int.Parse(Console.ReadLine());
+
+                string resultado = (num1 < num2) ? "CRESCENTE!" : "DECRESCENTE!";
+                Console.WriteLine(resultado);
+            } while (num1 != num2);
+        }
+    }
+}
