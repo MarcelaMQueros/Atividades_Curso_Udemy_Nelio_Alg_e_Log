62af4b1e552f873f1ac3a3f0bb41a50e34adec96
Marcela Marcellino Querós <143046716+MarcelaMQueros@users.noreply.github.com>
2024-09-21 21:32:08 -0300

    Add files via upload

---
+++ C#/Diagonal_negativos/Diagonal_negativos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Diagonalnegativos {
+    class Diagonalnegativos {
+        static void Main(String[] args) {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            int n, cont;
+            Console.Write("Qual a ordem da matriz? ");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n,n];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++){
+                    Console.Write($"Elemento [{i},{j}]: ");
+                    matriz[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+            cont = 0;
+            Console.WriteLine("DIAGONAL PRINCIPAL: ");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++){
+                    if (i == j) {
+                        Console.Write($"{matriz[i,j]} ");
+                    }
+                    if (matriz[i,j] < 0) {
+                        cont++;
+                    }
+                }
+            }
+
+            Console.Write($"\nQUANTIDADE DE NEGATIVOS = {cont}");
+        }
+    }
+}
