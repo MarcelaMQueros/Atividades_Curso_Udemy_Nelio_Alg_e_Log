109eec5a7ca21dd2069212f0c630508204918742
Marcela Marcellino Querós <143046716+MarcelaMQueros@users.noreply.github.com>
2024-09-21 21:17:02 -0300

    Add files via upload

---
+++ C#/Soma_vetor/Soma_vetor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Somavetor {
+    class Somavetor {
+        static void Main(String[] args) {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            int n;
+            double soma, media;
+            Console.Write("Quantos numeros voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            for (int i = 0; i < n; i++) {
+                Console.Write("Digite um numero: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+            }
+            soma = 0;
+            Console.Write("Valores = ");
+            for (int i = 0; i < n; i++) {
+                Console.Write($"{vet[i].ToString("F1", CI)} ");
+                soma += vet[i];
+            }
+            media = soma / n;
+            Console.Write($"\nSOMA = {soma.ToString("F1", CI)}\nMEDIA = {media.ToString("F1", CI)}");
+        }
+    }
+}
